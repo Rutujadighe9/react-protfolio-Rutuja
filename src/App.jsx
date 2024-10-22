5412a9ee00fc048c38b546ae7e81eca77fd9a301
Rutuja Dighe <rutujadighe9@gmail.com>
2024-10-22 20:18:50 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from './component/Navbar'
-// import Hero from './component/Hero'
-import About from './component/about'
+import About  from './component/About'
  import Contact from './component/Contact'
 import Technology from'./component/Technology'
 import Project from './component/project'
